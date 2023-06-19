d682d5ea58f8e2f459afcda40a5a65360777df6b
kevit <kevit.hardik.mehta@gmail.com>
2023-06-19 18:03:53 +0530

    account file chahnge

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,5 @@
 const sgMail = require('@sendgrid/mail');
 
-// const sendGridAPIKey = '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'
 
 sgMail.setApiKey(process.env.SG_API_KEY);
 
